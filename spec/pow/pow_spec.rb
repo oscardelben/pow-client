a4a8dbe84857ecb195e0b2503a951b795e56e91d
paranoiase Kang <paranoiase@gmail.com>
2012-04-07 14:52:46 +0900

    remove duplication of pow spec

--- spec/pow/pow_spec.rb
+++ spec/pow/pow_spec.rb
@@ -1,87 +1,94 @@
 require 'spec_helper'
 
-describe "Parser" do
+describe Pow do
+  def current_dir
+    `pwd`.chomp
+  end
 
-  context "adding a project" do
+  def default_appname
+    `pwd`.chomp.split('/').last
+  end
 
-    it "should call Pow::Add and pass the arguments" do
-      Pow::Add.should_receive(:new).with('.', 'myapp')
-      Pow::Parser .new %w{add . myapp}
+  describe "Parser" do
+
+    context "adding a project" do
+
+      it "should call Pow::Add and pass the arguments" do
+        Pow::Add.should_receive(:new).with('.', 'myapp')
+        Pow::Parser .new %w{add . myapp}
+      end
+
+      it "should call remove and pass the arguments" do
+        Pow::Remove.should_receive(:new).with('google')
+        Pow::Parser.new %w{remove google}
+      end
+
+      it "should call open and pass the arguments" do
+        Pow::Open.should_receive(:new).with('basecamp')
+        Pow::Parser.new %w{open basecamp}
+      end
+
+      it "should call restart" do
+        Pow::Restart.should_receive(:new)
+        Pow::Parser.new %w{restart}
+      end
+
+      it "should print USAGE if no match is found" do
+        $stdout.should_receive(:puts).with(Pow::USAGE)
+        Pow::Parser.new %w{sds}
+      end
     end
 
-    it "should call remove and pass the arguments" do
-      Pow::Remove.should_receive(:new).with('google')
-      Pow::Parser.new %w{remove google}
+  end
+
+  describe "Add" do
+    it "should expand the current path" do
+      Pow::Runner.should_receive(:run).with "ln -s #{current_dir} #{Pow::POW_DIR}"
+      Pow::Add.new
     end
 
-    it "should call open and pass the arguments" do
-      Pow::Open.should_receive(:new).with('basecamp')
-      Pow::Parser.new %w{open basecamp}
+    it "should expand the current path" do
+      Pow::Runner.should_receive(:run).with "ln -s #{current_dir} #{Pow::POW_DIR}"
+      Pow::Add.new '.'
     end
 
-    it "should call restart" do
-      Pow::Restart.should_receive(:new)
-      Pow::Parser.new %w{restart}
-    end
-
-    it "should print USAGE if no match is found" do
-      $stdout.should_receive(:puts).with(Pow::USAGE)
-      Pow::Parser.new %w{sds}
+    it "should use the specified path and app name" do
+      Pow::Runner.should_receive(:run).with "ln -s /a/path #{Pow::POW_DIR}appname"
+      Pow::Add.new '/a/path', 'appname'
     end
   end
 
-end
+  describe "Remove" do
+    it "should remove the app in the current directory" do
+      appname = default_appname
+      Pow::Runner.should_receive(:run).with "rm #{Pow::POW_DIR}#{appname}"
+      Pow::Remove.new
+    end
 
-describe "Add" do
-  it "should expand the current path" do
-    current_dir = `pwd`.chomp
-    Pow::Runner.should_receive(:run).with "ln -s #{current_dir} #{Pow::POW_DIR}"
-    Pow::Add.new
+    it "should remove the app" do
+      Pow::Runner.should_receive(:run).with "rm #{Pow::POW_DIR}google"
+      Pow::Remove.new 'google'
+    end
   end
 
-  it "should expand the current path" do
-    current_dir = `pwd`.chomp
-    Pow::Runner.should_receive(:run).with "ln -s #{current_dir} #{Pow::POW_DIR}"
-    Pow::Add.new '.'
+  describe "Open" do
+    it "should open the app in the current directory" do
+      appname = default_appname
+      Pow::Runner.should_receive(:run).with "open http://#{appname}.dev"
+      Pow::Open.new
+    end
+
+    it "should open the specified path" do
+      appname = 'rubyxp'
+      Pow::Runner.should_receive(:run).with "open http://#{appname}.dev"
+      Pow::Open.new appname
+    end
   end
 
-  it "should use the specified path and app name" do
-    Pow::Runner.should_receive(:run).with "ln -s /a/path #{Pow::POW_DIR}appname"
-    Pow::Add.new '/a/path', 'appname'
-  end
-end
-
-describe "Remove" do
-  it "should remove the app in the current directory" do
-    appname = `pwd`.chomp.split('/').last
-    Pow::Runner.should_receive(:run).with "rm #{Pow::POW_DIR}#{appname}"
-    Pow::Remove.new
-  end
-
-  it "should remove the app" do
-    Pow::Runner.should_receive(:run).with "rm #{Pow::POW_DIR}google"
-    Pow::Remove.new 'google'
-  end
-end
-
-describe "Open" do
-  it "should open the app in the current directory" do
-    appname = `pwd`.chomp.split('/').last
-    Pow::Runner.should_receive(:run).with "open http://#{appname}.dev"
-    Pow::Open.new
-  end
-
-  it "should open the specified path" do
-    appname = 'rubyxp'
-    Pow::Runner.should_receive(:run).with "open http://#{appname}.dev"
-    Pow::Open.new appname
+  describe "Restart" do
+    it "should restart the app" do
+      Pow::Runner.should_receive(:run).with "touch tmp/restart.txt"
+      Pow::Restart.new
+    end
   end
 end
-
-describe "Restart" do
-  it "should restart the app" do
-    Pow::Runner.should_receive(:run).with "touch tmp/restart.txt"
-    Pow::Restart.new
-  end
-end
-
